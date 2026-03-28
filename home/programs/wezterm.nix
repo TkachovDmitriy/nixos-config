a35297dbc3df3da402fc84dbd47e4b740692e0a5
dtkachov <d.tkachov982@gmail.com>
2026-03-28 13:51:12 +0200

    [Feature] create nixos config for my setup

--- home/programs/wezterm.nix
+++ home/programs/wezterm.nix
@@ -14,6 +14,8 @@
       config.window_background_opacity    = 0.95
       config.hide_tab_bar_if_only_one_tab = true
       config.default_prog = { "zsh" }
+      config.window_decorations = "RESIZE"
+      config.window_frame = { border_left_width = "4px", border_right_width = "4px", border_bottom_height = "4px", border_top_height = "4px" }
 
       config.keys = {
         { key = "t", mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab "CurrentPaneDomain" },
